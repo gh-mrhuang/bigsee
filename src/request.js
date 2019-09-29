0218add51ad308aa79a01b61fe00b9a709f2ed17
harry.huang <harry.huang@cn.shijigroup.com>
2019-09-29 22:23:12 +0800

    添加axios

---
+++ src/request.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+
+// 创建axios 实例
+const service = axios.create({
+  baseURL: window.location.protocol + '//' + window.location.host,
+})
+
+service.interceptors.request.use(
+  config => {
+    // 中英文标识
+    config.headers['Accept-Language'] = 'zh'
+    return config
+  },
+  error => {
+    Promise.reject(error)
+  }
+)
+
+service.interceptors.response.use(
+  response => {
+    const result = response.data
+    return result.data === undefined ? result : result.data
+  },
+  error => {
+    Promise.reject(error)
+  }
+)
+
+export default service
