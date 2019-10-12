5f5132e71e7e63992e79555ffdd3611670fdb980
gangking <gang.xiong1@pactera.com>
2019-10-12 23:49:29 +0800

    改大

--- src/page/XgAboutUs.vue
+++ src/page/XgAboutUs.vue
@@ -141,7 +141,7 @@ export default {
   }
 }
 .imgsz {
-  width: 30%;
+  width: 35%;
   display: inline-block;
   position: relative;
   vertical-align: middle;
@@ -165,7 +165,7 @@ export default {
   }
 }
 .imgcs {
-  margin-left: 40%;
+  margin-left: 30%;
 }
 .mobile-moreimg {
   width: 100%;
